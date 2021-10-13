25d43728acc2f391a3b139c02e79e8f78edcdf96
Johann von Tiesenhausen <blah424@gmail.com>
2021-10-13 16:36:38 -0400

    fix editor font size issue

--- js/editor.js
+++ js/editor.js
@@ -2,5 +2,5 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night");
 editor.session.setMode("ace/mode/lua");
 editor.setShowPrintMargin(false);
-
+editor.setFontSize(16);
 editor.resize();
